a88406217431e815691fb22f42c8f5ce4ccdb2bd
laquino <akku@outlook.com>
2016-02-03 23:10:40 +0100

    removed unnecessary rootScope imports

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -7,17 +7,17 @@
  * # LoginCtrl
  * Controller of the onlinesenpaiUiApp
  */
-angular.module('onlinesenpaiUiApp').controller('LoginCtrl', function ($scope, $rootScope, $location, SecurityService) {
-  if ($rootScope.isLoggedIn()) {
+angular.module('onlinesenpaiUiApp').controller('LoginCtrl', function ($scope, $location, SecurityService) {
+  if ($scope.isLoggedIn()) {
     $location.path('/techniques');
   }
-    
+
   $scope.user = {mail:'', password: ''};
-  
+
   $scope.login = function() {
     SecurityService.login($scope.user).$promise.then(
         function(result) {
-            $rootScope.setCredentials(result.user, result.token.token);
+            $scope.setCredentials(result.user, result.token.token);
             $location.path('/techniques');
         }, function(error) {
             console.log('login failed');
--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -7,19 +7,18 @@
  * # SignupCtrl
  * Controller of the onlinesenpaiUiApp
  */
-angular.module('onlinesenpaiUiApp').controller('SignUpCtrl', function ($scope, $routeParams, $location, $rootScope, SecurityService) {
+angular.module('onlinesenpaiUiApp').controller('SignUpCtrl', function ($scope, $routeParams, $location, SecurityService) {
     if (!$routeParams.token) {
         $location.path('/login');
-    } else {
-        localStorage.setItem('token', $routeParams.token);
     }
-    
+
     $scope.user = {password: ''}
-    
+
     $scope.signup = function() {
+      localStorage.setItem('token', $routeParams.token);
       SecurityService.signup($scope.user).$promise.then(function(result) {
-        $rootScope.setCredentials(result.user, result.token.token);
+        $scope.setCredentials(result.user, result.token.token);
         $location.path('/techniques');
-      });  
-    };    
+      });
+    };
 });
